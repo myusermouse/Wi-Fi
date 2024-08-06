8f93b4cafb93c4f9490e3698efae5aa88046ea1e
rahul253040 <mdrahulsarkar5525@gmail.com>
2024-08-06 17:31:07 +0600

    update

---
+++ main.js
@@ -0,0 +1,6 @@
+const forgetClick = document.querySelector('.forget-click');
+const click = document.querySelector('.click');
+
+click.addEventListener('click',()=>{
+    forgetClick.classList.toggle('pp');
+},1000)
